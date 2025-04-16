9f1d033f5abc561beaca1c129e67d950ff91c067
apaixaols <140622198+apaixaols@users.noreply.github.com>
2025-04-16 11:48:41 +0000

    ok

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-document.addEventListener('DOMContentLoaded' , function(){
+document,addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+      opcoesAcessibilidade.classList.toggle('apresenta-lista');
+    })
+    
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-   
-   aumentaFonteBotao.addEventListener('click', function(){
-       tamanhoAtualFonte +=0.1;
-       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-   })
-   diminuirFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte -=0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-})
-})
 
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -=0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    }) 
+})
 
